fbb781fc2d6fc4f7e364a3942423df6fe7e18fce
Sep Keuchenius <sep@y.digital>
2025-01-27 11:38:18 +0100

    add prom stats

---
+++ app/routes/prometheusstats.tsx
@@ -0,0 +1,55 @@
+import { LoaderFunctionArgs } from "@remix-run/node";
+import { EloStanding, getElos, getElosAsUserStats, getMatches, getUserNames } from "~/services/firebase.server";
+import { UserStats } from "./dashboard/route";
+import { calcEloFromGames, getTimeFrameByScope, listGamesInTimeFrame as listMatchesInTimeFrame } from "~/utils/calc_elo";
+
+
+const LEARNING_RATE_DEFAULT = 4
+
+export async function loader({ request, params }: LoaderFunctionArgs) {
+
+    const timeScope = "alltime"
+    const timeIndex = 0
+
+    const elos = await getElosAsUserStats();
+
+    const userNames = await getUserNames();
+
+    const matches = await getMatches()
+    let { searchParams } = new URL(request.url);
+    const learningRate = Number(searchParams.get("learningrate") || LEARNING_RATE_DEFAULT);
+
+
+    const [startTime, endTime] = getTimeFrameByScope(timeScope, timeIndex);
+    const matchesInTimeFrame = listMatchesInTimeFrame(matches, startTime, endTime);
+    const lastElos = calcEloFromGames(matchesInTimeFrame, learningRate);
+
+    let lastMatchPlayers = new Set()
+    for (const matchI in matches) {
+        const index = Number(matchI)
+        const match = matches[index]
+        if (elos[index] && index > 0) {
+            const prevElo = elos[index - 1]
+            const changes = elos[index].map((standing: UserStats) => {
+                const prevStanding = prevElo.find((prevStanding: UserStats) => prevStanding.userId == standing.userId)
+                return {
+                    ...standing,
+                    eloChange: standing.elo - (prevStanding ? prevStanding.elo : 0)
+                }
+            }).filter((standing: UserStats) => standing.eloChange != 0).map((standing: UserStats) => { return userNames[standing.userId].name })
+            const match = matches[index]
+            lastMatchPlayers = new Set([userNames[match.loser].name, userNames[match.winner].name])
+        }
+        else {
+            lastMatchPlayers = new Set([userNames[match.loser].name, userNames[match.winner].name])
+        }
+
+    }
+
+    return new Response(
+        lastElos.map((standing: UserStats) => {
+            return `${userNames[standing.userId].name} ${standing.elo}`
+        }).join("\n"), 
+        {headers:{ "Content-Type": "text/plain" }}
+    );
+}
